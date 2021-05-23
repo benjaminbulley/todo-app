dc4926711ffea0ed822d68c785cdc575628e3013
Ben Bulley <benjaminbulley@gmail.com>
2021-05-23 12:50:34 +0100

    Setting up Todo App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import TodoItem from './TodoItem';
+import todosData from "./todosData";
 
 function App() {
+  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          **TODO**
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="todo-list">
+        {todoItems}
+      </div>
     </div>
   );
 }
