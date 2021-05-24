fc38bff740b846e9ec28ff80ac619eed31556ab1
Ben Bulley <benjaminbulley@gmail.com>
2021-05-24 18:16:39 +0100

    Implementing stateful class component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import './App.css';
 import TodoItem from './TodoItem';
 import todosData from "./todosData";
 
-function App() {
-  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          **TODO**
-        </p>
-      </header>
-      <div className="todo-list">
-        {todoItems}
+class App extends React.Component {
+  constructor(){
+      super();
+      this.state = {
+        todos: todoItems
+      }
+  }
+  render(){
+      const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+      return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            **TODO**
+          </p>
+        </header>
+        <div className="todo-list">
+          {todoItems}
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
+  
 }
 
 export default App;
